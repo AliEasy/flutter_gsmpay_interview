66f4e77277cb4cd578b8d8e6660dc4e5b7b68093
Ali <ali.ezad77@gmail.com>
2025-07-26 17:40:02 +0330

    add submit button to home page

--- lib/features/home/presentation/page/home_page.dart
+++ lib/features/home/presentation/page/home_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gsmpay_interview/core/di/base/di_setup.dart';
 import 'package:flutter_gsmpay_interview/core/uikit/box_partition_with_header.dart';
+import 'package:flutter_gsmpay_interview/core/uikit/button_widget.dart';
 import 'package:flutter_gsmpay_interview/core/uikit/image_picker_box_widget.dart';
 import 'package:flutter_gsmpay_interview/core/uikit/scaffold_body_root_widget.dart';
+import 'package:flutter_gsmpay_interview/core/uikit/scaffold_bottom_navigator_bar_root_widget.dart';
 import 'package:flutter_gsmpay_interview/core/uikit/text_form_field_widget.dart';
+import 'package:flutter_gsmpay_interview/core/uikit/toast.dart';
+import 'package:flutter_gsmpay_interview/features/home/domain/entity/submit_info_request_entity.dart';
 import 'package:flutter_gsmpay_interview/features/home/presentation/manager/form_validator/home_form_validator_cubit.dart';
+import 'package:flutter_gsmpay_interview/features/home/presentation/manager/submit_info/home_submit_info_cubit.dart';
 import 'package:flutter_gsmpay_interview/features/home/presentation/manager/upload_birth_certificate_image/upload_birth_certificate_image_cubit.dart';
 import 'package:flutter_gsmpay_interview/features/home/presentation/manager/upload_national_id_image/upload_national_id_image_cubit.dart';
 import 'package:flutter_gsmpay_interview/generated/l10n.dart';
@@ -34,6 +39,7 @@ class _HomePageState extends State<HomePage> {
         BlocProvider(create: (context) => getIt<HomeFormValidatorCubit>()),
         BlocProvider(create: (context) => getIt<UploadNationalIdImageCubit>()),
         BlocProvider(create: (context) => getIt<UploadBirthCertificateImageCubit>()),
+        BlocProvider(create: (context) => getIt<HomeSubmitInfoCubit>()),
       ],
       child: MultiBlocListener(
         listeners: [
@@ -42,6 +48,9 @@ class _HomePageState extends State<HomePage> {
               state.whenOrNull(
                 success: (imageId) {
                   _nationalIdImageId = imageId;
+                  context.read<HomeFormValidatorCubit>().validateNationalIdImageId(
+                    imageId,
+                  );
                 },
               );
             },
@@ -54,6 +63,18 @@ class _HomePageState extends State<HomePage> {
               state.whenOrNull(
                 success: (imageId) {
                   _birthCertificatedImageId = imageId;
+                  context
+                      .read<HomeFormValidatorCubit>()
+                      .validateBirthCertificateImageId(imageId);
+                },
+              );
+            },
+          ),
+          BlocListener<HomeSubmitInfoCubit, HomeSubmitInfoState>(
+            listener: (context, state) {
+              state.whenOrNull(
+                success: (record) {
+                  showSuccessToast(context, record);
                 },
               );
             },
@@ -94,7 +115,7 @@ class _HomePageState extends State<HomePage> {
                                 validator: (value) {
                                   return context
                                       .read<HomeFormValidatorCubit>()
-                                      .validateFullName(value ?? '');
+                                      .validateNationalCode(value ?? '');
                                 },
                               ),
                               const SizedBox(height: 20),
@@ -137,6 +158,7 @@ class _HomePageState extends State<HomePage> {
                                         _nationalIdImageId = null;
                                         _nationalIdImage = null;
                                       },
+                                      required: true,
                                     );
                                   },
                                 ),
@@ -164,6 +186,7 @@ class _HomePageState extends State<HomePage> {
                                         _birthCertificatedImageId = null;
                                         _birthCertificatedImage = null;
                                       },
+                                      required: true,
                                     );
                                   },
                                 ),
@@ -173,6 +196,74 @@ class _HomePageState extends State<HomePage> {
                         ),
                       ],
                     ),
+                  ),
+                ),
+                bottomNavigationBar: ScaffoldBottomNavigatorBarRootWidget(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: BlocBuilder<
+                          HomeFormValidatorCubit,
+                          HomeFormValidatorState
+                        >(
+                          builder: (context, state) {
+                            return state.whenOrNull(
+                                  invalid: () {
+                                    return ButtonWidget(
+                                      title: S.of(context).final_submit,
+                                      buttonType: ButtonType.filled,
+                                    );
+                                  },
+                                ) ??
+                                BlocBuilder<
+                                  HomeSubmitInfoCubit,
+                                  HomeSubmitInfoState
+                                >(
+                                  builder: (context, state) {
+                                    return state.whenOrNull(
+                                          loading: () {
+                                            return ButtonWidget(
+                                              title: S.of(context).final_submit,
+                                              buttonType: ButtonType.filled,
+                                              isLoading: true,
+                                            );
+                                          },
+                                        ) ??
+                                        ButtonWidget(
+                                          title: S.of(context).final_submit,
+                                          buttonType: ButtonType.filled,
+                                          icon: const Icon(Icons.done),
+                                          onPressed: () {
+                                            context
+                                                .read<HomeSubmitInfoCubit>()
+                                                .submit(
+                                                  SubmitInfoRequestEntity(
+                                                    name: _fullNameController.text,
+                                                    nationalCode:
+                                                        _nationalCodeController.text,
+                                                    salary:
+                                                        int.tryParse(
+                                                          _monthlySalaryController
+                                                              .text
+                                                              .replaceAll(',', ''),
+                                                        ) ??
+                                                        0,
+                                                    nationalCardImageId:
+                                                        _nationalIdImageId
+                                                            .toString(),
+                                                    birthCertificateImageId:
+                                                        _birthCertificatedImageId
+                                                            .toString(),
+                                                  ),
+                                                );
+                                          },
+                                        );
+                                  },
+                                );
+                          },
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
